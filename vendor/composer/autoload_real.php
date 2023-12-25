88aa5134dd1023c1c66da23c19d609d51bdec494
MatseVH <49431416+Matse2005@users.noreply.github.com>
2023-12-25 23:16:34 +0100

    Removed unused packages

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,8 +22,6 @@ class ComposerAutoloaderInit93329acba776cac12f8340bcbf1089a4
             return self::$loader;
         }
 
-        require __DIR__ . '/platform_check.php';
-
         spl_autoload_register(array('ComposerAutoloaderInit93329acba776cac12f8340bcbf1089a4', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInit93329acba776cac12f8340bcbf1089a4', 'loadClassLoader'));
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -20,16 +20,6 @@ class ComposerStaticInit93329acba776cac12f8340bcbf1089a4
         ),
     );
 
-    public static $prefixesPsr0 = array (
-        'P' => 
-        array (
-            'Parsedown' => 
-            array (
-                0 => __DIR__ . '/..' . '/erusev/parsedown',
-            ),
-        ),
-    );
-
     public static $classMap = array (
         'Composer\\InstalledVersions' => __DIR__ . '/..' . '/composer/InstalledVersions.php',
     );
@@ -39,7 +29,6 @@ class ComposerStaticInit93329acba776cac12f8340bcbf1089a4
         return \Closure::bind(function () use ($loader) {
             $loader->prefixLengthsPsr4 = ComposerStaticInit93329acba776cac12f8340bcbf1089a4::$prefixLengthsPsr4;
             $loader->prefixDirsPsr4 = ComposerStaticInit93329acba776cac12f8340bcbf1089a4::$prefixDirsPsr4;
-            $loader->prefixesPsr0 = ComposerStaticInit93329acba776cac12f8340bcbf1089a4::$prefixesPsr0;
             $loader->classMap = ComposerStaticInit93329acba776cac12f8340bcbf1089a4::$classMap;
 
         }, null, ClassLoader::class);
